a1d4243c35c666dd0061fe5ee0fd13c0643a93cb
Beom June Shim <bshim4@illinois.edu>
2016-02-20 11:26:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,6 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('TIM SUCKS =P\n' +'jk i love you baby');
-  
-  <!DOCTYPE html>
-<html>
-<body>
 
-<h1>My First JavaScript</h1>
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
-
-<p id="demo"></p>
-
-</body>
-</html> 
 
 }).listen(port);
