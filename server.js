1764cc27561e0d5590b119645096d9394ab94d6e
intrepiduiuc <nccho2@illinois.edu>
2016-02-20 11:56:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/bold' });
-  res.end('TIM SUCKS =P\n' +'jk i love you baby');
+res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
   res.write('<!doctype html>\n<html lang="en">\n' + 
     '\n<meta charset="utf-8">\n<title>Test web</title>\n' + 
     '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
     '\n\n<h1>TIM IS A SUCKER</h1>\n' + 
     '<div id="content"><p>HARRY IS ALSO A SUCKER</p></div>' + 
     '\n\n');
+  res.end();
   console.log("Hello World!");
 }).listen(port);
