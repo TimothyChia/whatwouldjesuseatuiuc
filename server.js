f2f1865de3618d890a867d9b0c9124966806c8ad
Beom June Shim <bshim4@illinois.edu>
2016-02-20 11:08:59 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('TIM SUCKS =P\n' +'jk i love you baby');
+  res.end('TIM SUCKS =P\n' +'jk i love you baby'+ 'https://courses.engr.illinois.edu/cs125/eclipse.php');
 }).listen(port);
