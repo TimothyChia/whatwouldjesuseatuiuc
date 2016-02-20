a62f2c47b1be236c15fbe5c74325b93d1ba876a8
intrepiduiuc <nccho2@illinois.edu>
2016-02-20 12:00:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ res.writeHead(200, {
     'Content-Type': 'text/html'
   });
   res.write('<!doctype html>\n<html lang="en">\n' + 
-    '\n<meta charset="utf-8">\n<title>Test web</title>\n' + 
-    '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
-    '\n\n<h1>TIM IS A SUCKER</h1>\n' + 
-    '<div id="content"><p>HARRY IS ALSO A SUCKER</p></div>' + 
+    '\n<meta charset="utf-8">\n<title>WWJE UIUC</title>\n' + 
+    '<style type="text/css">* {font-family:Verdana,sans-serif;}</style>\n' + 
+    '\n\n<h1>THE WORKERS</h1>\n' + 
+    '<div id="content"><p>TIM works on like 50%, HENRY works on 35%, NATHAN works on 15%</p></div>' + 
     '\n\n');
   res.end();
   console.log("Hello World!");
