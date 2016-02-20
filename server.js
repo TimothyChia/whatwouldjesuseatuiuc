679daf63103b967d99f8f7b62b989e45f1a37fc4
Beom June Shim <bshim4@illinois.edu>
2016-02-20 11:22:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,21 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('TIM SUCKS =P\n' +'jk i love you baby'+ 'https://courses.engr.illinois.edu/cs125/eclipse.php');
+  res.end('TIM SUCKS =P\n' +'jk i love you baby');
+  
+  <!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First JavaScript</h1>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
+
 }).listen(port);
