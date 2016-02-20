7c0bb52b4af6b634fa587a3632e7327d2b9aac24
Beom June Shim <bshim4@illinois.edu>
2016-02-20 11:40:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/bold' });
-  res.end('TIM SUCKS =P\n' +'jk i love you baby');
-
-<button onclick="play_audio('play')">PLAY</button>
-<button onclick="play_audio('stop')">STOP</button>
-}).listen(port);
+const http = require('http');
+var keepAliveAgent = new http.Agent({ keepAlive: true });
+options.agent = keepAliveAgent;
+http.request(options, onResponseCallback);
