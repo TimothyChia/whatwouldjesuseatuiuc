8032a7df1c9a7a5b38b31d0d79cb2b1405ce5253
intrepiduiuc <nccho2@illinois.edu>
2016-02-20 12:04:58 -0600

    changing to folder

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  res.write('<!doctype html>\n<html lang="en">\n' + 
-    '\n<meta charset="utf-8">\n<title>WWJE UIUC</title>\n' + 
-    '<style type="text/css">* {font-family:Verdana,sans-serif;}</style>\n' + 
-    '\n\n<h1>THE WORKERS</h1>\n' + 
-    '<div id="content"><p>TIM works on like 50%, HENRY works on 35%, NATHAN works on 15%</p></div>' + 
-    '\n\n');
-  res.end();
-  console.log("Hello World!");
-}).listen(port);
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/html'));
