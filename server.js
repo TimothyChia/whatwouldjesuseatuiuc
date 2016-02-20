919832111d8f5ed4b1d9619a4e3d8ba13098dd25
Beom June Shim <bshim4@illinois.edu>
2016-02-20 11:34:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(500, { 'Content-Type': 'text/bold' });
+  res.writeHead(200, { 'Content-Type': 'text/bold' });
   res.end('TIM SUCKS =P\n' +'jk i love you baby');
 
-
+<button onclick="play_audio('play')">PLAY</button>
+<button onclick="play_audio('stop')">STOP</button>
 }).listen(port);
